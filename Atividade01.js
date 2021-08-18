17a1e298df6d6f79aa31bcbcc191270fc60c31d2
Rebeca <emi.ito@edu.unifil.br>
2021-08-18 15:41:55 -0300

    Arrumando o formato

---
+++ Atividade01.js
@@ -0,0 +1,7 @@
+function Calcular(){
+    var xUm = document.getElementById("x1");
+    var xDois = document.getElementById("x2");
+    var xTres = document.getElementById("x3");
+    var xQuatro = document.getElementById("x4");
+    var xCinco = document.getElementById("x5");
+}
