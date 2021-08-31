169b88deabbd9c47894f769ccdeb445891ea66e2
Rebeca <emi.ito@edu.unifil.br>
2021-08-31 17:10:10 -0300

    Finalizado

--- Projeto2/Projeto2.js
+++ Projeto2/Projeto2.js
@@ -1,36 +1,89 @@
 function Calcular(){
 
-    const wUm = document.getElementById("w1").value;
     const xUm = document.getElementById("x1").value;
     const yUm = document.getElementById("y1").value;
     const zUm = document.getElementById("z1").value;
+    const bUm = document.getElementById("b1").value;
     
-    const wDois = document.getElementById("w2").value;
     const xDois = document.getElementById("x2").value;
     const yDois = document.getElementById("y2").value;
     const zDois = document.getElementById("z2").value;
+    const bDois = document.getElementById("b2").value;
     
-    const wTres = document.getElementById("w3").value;
     const xTres = document.getElementById("x3").value;
     const yTres = document.getElementById("y3").value;
     const zTres = document.getElementById("z3").value;
+    const bTres = document.getElementById("b3").value;
 
-    const wQuatro = document.getElementById("w4").value;
-    const xQuatro = document.getElementById("x4").value;
-    const yQuatro = document.getElementById("y4").value;
-    const zQuatro = document.getElementById("z4").value;
-
-    var linha1 = [wUm, xUm, yUm, zUm];
-    var linha2 = [wDois, xDois, yDois, zDois];
-    var linha3 = [wTres, xTres, yTres, zTres];
-    var linha4 = [wQuatro, xQuatro, yQuatro, zQuatro];
-
-    Resolver(linha1, linha2, linha3, linha4);
+    var linha1 = [xUm, yUm, zUm, bUm];
+    var linha2 = [xDois, yDois, zDois, bDois];
+    var linha3 = [xTres, yTres, zTres, bTres];
+    
+    Resolver(linha1, linha2, linha3);
 }
 
-function Resolver(linha1, linha2, linha3, linha4){
-    
-    for (var i = 0; i < 4; i++) {
+function Resolver(linha1, linha2, linha3){
+    var matriz = [linha1, linha2, linha3];
+    var pivo = 0;
 
+    var j = 0;
+    var k = 0;
+    
+    do{
+        var i = 1;
+        if (k == 1){
+            i = 2
+        }
+
+        for (; i < 3; i++) {
+            var a = matriz[k][j];
+            var b = matriz[i][j];
+            pivo = -(b/a);
+            
+            for (var l = 0; l < 4; l++){
+                matriz[k][l] = matriz[k][l] * pivo;
+                matriz[i][l] = matriz[i][l] * pivo;
+                matriz[i][j] = 0;
+            }
+            addNaTabela(pivo, matriz);
+        }
+        j += 1;
+        k += 1;
+    }while(k < 2);
+}
+//  1 2 3 = 85
+//  0 5 6 = 45
+//  0 8 9 = 65
+
+function matrizParaString(matriz){
+    var mString = "";
+    for (var i = 0; i < 3; i++){
+        for (var j = 0; j < 4; j++){
+            var a = matriz[i][j].toString();
+            if (j == 0){
+                mString = [mString + "[(" + a +")"].toString();
+            }else if(j == 3){
+                mString = [mString + " = " + a].toString();
+            }else{
+                mString = [mString + "+ (" + a + ") "].toString();
+            }
+        }
+        mString = [mString + "]"]
     }
+
+    return mString;
+}
+
+function addNaTabela(pivo, matriz){
+    var mString = matrizParaString(matriz);
+    
+    var table = document.getElementById("table-body");
+    var totalRowCount = table.rows.length;
+    var row = table.insertRow(totalRowCount);
+    
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+
+    cell1.innerHTML = pivo;
+    cell2.innerHTML = mString;
 }
