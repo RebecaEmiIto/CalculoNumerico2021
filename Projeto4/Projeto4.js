433f2ce6c8690b77ec65afc6fb5fcd69fc152c73
Rebeca <emi.ito@edu.unifil.br>
2021-11-10 20:39:14 -0300

    JS + Interface

---
+++ Projeto4/Projeto4.js
@@ -0,0 +1,22 @@
+function Calcular(){
+
+    const xZero = document.getElementById("xZero").value;
+    const yZero = document.getElementById("yZero").value;
+
+    const a = document.getElementById("a").value;
+    const b = document.getElementById("b").value;
+    const n = document.getElementById("n").value;
+    var altura = (b - a) / n;
+
+    
+}
+
+function regraTrapezio(){
+}
+
+/**
+ * na entrada vc deve escrever a integral
+    ou seja
+    a função, o intervalo e o valor de n
+    certo?
+ */
