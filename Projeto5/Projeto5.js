96f99d2ea98e7bcdcde9b178522c7fd895473696
Rebeca <emi.ito@edu.unifil.br>
2021-11-13 16:53:24 -0300

    iniciando Projeto

---
+++ Projeto5/Projeto5.js
@@ -0,0 +1,26 @@
+function Calcular(){
+    /* função */
+    const xCinco = document.getElementById("x5").value;
+    const yQuatro = document.getElementById("x4").value;
+    const xTres = document.getElementById("x3").value;
+    const xDois = document.getElementById("x2").value;
+    const xUm = document.getElementById("x1").value;
+    const c = document.getElementById("c").value;
+
+    const a = document.getElementById("a").value; /* ponto a (início do intervalo) */
+    const b = document.getElementById("b").value; /* ponto b (final do intervalo) */
+    const n = document.getElementById("n").value; /* quantidade de intervalos */
+    var altura = (b - a) / n;
+
+    
+}
+
+function regraTrapezio(){
+}
+
+/**
+ * na entrada vc deve escrever a integral
+    ou seja
+    a função, o intervalo e o valor de n
+    certo?
+ */
