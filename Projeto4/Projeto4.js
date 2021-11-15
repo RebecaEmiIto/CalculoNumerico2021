79769186913e06581d78149f840eabb3bac1f3ac
Rebeca <emi.ito@edu.unifil.br>
2021-11-15 10:59:02 -0300

    criando tabela (não finalizado)

--- Projeto4/Projeto4.js
+++ Projeto4/Projeto4.js
@@ -63,8 +63,25 @@ function regraTrapezio(a, b, n, xCinco, xQuatro, xTres, xDois, xUm, c){
 
     //erro%
     erroCento = (erro / resultadoLog) * 100
+
+    addNaTabela(erro, erroCento)
 }
 
+function addNaTabela(x, y, erro, erroCento){
+    var table = document.getElementById("table-body");
+    var totalRowCount = table.rows.length;
+    var row = table.insertRow(totalRowCount);
+    
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    var cell4 = row.insertCell(3);
+
+    cell1.innerHTML = x;
+    cell2.innerHTML = y;
+    cell3.innerHTML = erro;
+    cell4.innerHTML = erroCento;
+}
 /**
  * na entrada vc deve escrever a integral
     ou seja
