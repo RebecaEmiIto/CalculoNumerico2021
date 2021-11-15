7f559fa0d035b3b4c03f53bdf317f20b577143bd
Rebeca <emi.ito@edu.unifil.br>
2021-11-15 12:51:27 -0300

    Implementando a Regra de Simpson (JS)

--- Projeto5/Projeto5.js
+++ Projeto5/Projeto5.js
@@ -1,8 +1,5 @@
 function Calcular(){
     /* função */
-    const xCinco = document.getElementById("x5").value;
-    const yQuatro = document.getElementById("x4").value;
-    const xTres = document.getElementById("x3").value;
     const xDois = document.getElementById("x2").value;
     const xUm = document.getElementById("x1").value;
     const c = document.getElementById("c").value;
@@ -10,17 +7,52 @@ function Calcular(){
     const a = document.getElementById("a").value; /* ponto a (início do intervalo) */
     const b = document.getElementById("b").value; /* ponto b (final do intervalo) */
     const n = document.getElementById("n").value; /* quantidade de intervalos */
-    var altura = (b - a) / n;
-
     
 }
 
-function regraTrapezio(){
+function regraSimpson(a, b, n, xDois, xUm, c){
+    var dxSoma = [0];
+    var arrX = [];
+    var passo = (b - a) / n;
+    var resultadoSomaDx = 0;
+
+    // Encontra os xs
+    for (let j = a; j <= b; j += passo){
+        arrX.push(j);
+    }
+
+    // Encontra os intervalos
+    for (let i = 0; i < arrX.length; i += 2){
+        h = ((arrX[i + 1]) - arrX[i]) / (2 * n);
+
+        y0 = ((xDois * arrX[i]^2) + (xUm * arrX[i]) + c);;
+        y1 = 4 * ((xDois * arrX[i + 1]^2) + (xUm * arrX[i + 1]) + c);
+        y2 = ((xDois * arrX[i + 2]^2) + (xUm * arrX[i + 2]) + c);;
+
+        dxSoma[i] = arrX[i] + y0;
+        dxSoma.push(y1);
+        dxSoma.push(y2);        
+    }
+
+    // Soma da lista "dxSoma"
+    for (let k = 0; k < dxSoma.length; k++){
+        resultadoSomaDx += dxSoma[k];
+    }
+    resultadoDx = (h/3) * resultadoSomaDx;
 }
 
-/**
- * na entrada vc deve escrever a integral
-    ou seja
-    a função, o intervalo e o valor de n
-    certo?
- */
+function addNaTabela(x, y, erro, erroCento){
+    var table = document.getElementById("table-body");
+    var totalRowCount = table.rows.length;
+    var row = table.insertRow(totalRowCount);
+    
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    var cell4 = row.insertCell(3);
+
+    cell1.innerHTML = x;
+    cell2.innerHTML = y;
+    cell3.innerHTML = erro;
+    cell4.innerHTML = erroCento;
+}
