c8b09ecb38217c9ed972554c0b5164dc524c5788
Rebeca <emi.ito@edu.unifil.br>
2021-11-15 17:37:23 -0300

    aprimorando o código

--- Projeto5/Projeto5.js
+++ Projeto5/Projeto5.js
@@ -3,11 +3,11 @@ function Calcular(){
     const xDois = parseInt(document.getElementById("x2").value);
     const xUm = parseInt(document.getElementById("x1").value);
     const c = parseInt(document.getElementById("c").value);
-
+    
     const a = parseInt(document.getElementById("a").value); /* ponto a (início do intervalo) */
     const b = parseInt(document.getElementById("b").value); /* ponto b (final do intervalo) */
     const n = parseInt(document.getElementById("n").value); /* quantidade de intervalos */
-
+    
     regraSimpson(a, b, n, xDois, xUm, c);
     
 }
@@ -15,8 +15,10 @@ function Calcular(){
 function regraSimpson(a, b, n, xDois, xUm, c){
     var dxSoma = [];
     var arrX = [];
+    var arrY = [];
     var passo = parseInt(((b - a) / n).toFixed());
     var resultadoSomaDx = 0;
+    h = (b - a) / (2 * n);
 
     // Encontra os xs
     for (var j = a; j <= b; j += passo){
@@ -30,38 +32,47 @@ function regraSimpson(a, b, n, xDois, xUm, c){
         y1 = 4 * ((xDois * Math.pow(arrX[i], 2)) + (xUm * arrX[i]) + c);
         y2 = ((xDois * Math.pow(arrX[i + 1], 2)) + (xUm * arrX[i + 1]) + c);
         
-        soma = y0 + y1 + y2;
+        soma = (y0 + y1 + y2);
         
+        arrY.push(y0);
+        arrY.push(y1);
+        arrY.push(y2);
         dxSoma.push(soma);
-        //dxSoma.push(y1);
-        //dxSoma.push(y2);
     }
     
-    h = (b - a) / (2 * n);
 
     // Soma da lista "dxSoma"
     for (var k = 0; k < dxSoma.length; k++){
         resultadoSomaDx += dxSoma[k];
     }
-    resultadoDx = (h/3) * resultadoSomaDx;
+    //resultadoDx = (h/3) * resultadoSomaDx;
     
-    //document.getElementById("teste").innerHTML = h;
+    document.getElementById("teste").innerHTML = resultadoSomaDx;
     
-    addNaTabela(a, b, n, resultadoDx);
+    addNaTabela(arrX, arrY, n, dxSoma);
 }
 
-function addNaTabela(a, b, n, resultadoDx){
+function addNaTabela(arrX, arrY, n, dxSoma){
     var table = document.getElementById("table-body");
     var totalRowCount = table.rows.length;
     var row = table.insertRow(totalRowCount);
-    
-    var cell1 = row.insertCell(0);
-    var cell2 = row.insertCell(1);
-    var cell3 = row.insertCell(2);
-    var cell4 = row.insertCell(3);
+    var contador = 0;
+    var resultadoIndex = 0;
 
-    cell1.innerHTML = a;
-    cell2.innerHTML = b;
-    cell3.innerHTML = n;
-    cell4.innerHTML = resultadoDx;
+    for (var i = 0; i <= arrX.length; i++){
+        var cell1 = row.insertCell(i);
+        var valor = 0;
+        if (contador < 3){
+            var x = arrX[i];
+            var y = arrY[i];
+            valor = (x + ", " + y);        
+        }else{
+            valor = dxSoma[resultadoIndex];
+            resultadoIndex++;
+            contador = 0;
+        }
+        contador++;
+        cell1.innerHTML = valor;
+    }
+
 }
