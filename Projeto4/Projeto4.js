e67a29868abddbfa10f849f119c769c4926ca995
Rebeca <emi.ito@edu.unifil.br>
2021-11-15 17:37:57 -0300

    implementando JS

--- Projeto4/Projeto4.js
+++ Projeto4/Projeto4.js
@@ -64,19 +64,24 @@ function regraTrapezio(a, b, n, xCinco, xQuatro, xTres, xDois, xUm, c){
     //erro%
     erroCento = (erro / resultadoLog) * 100
 
-    addNaTabela(erro, erroCento)
+    for (var l = 0; l < arrX.length; l++){
+        var xr = arrX[l];
+        var yr = arrY[l];
+        addNaTabela(xr, yr, erro, erroCento)
+    }
 }
 
 function addNaTabela(x, y, erro, erroCento){
     var table = document.getElementById("table-body");
     var totalRowCount = table.rows.length;
     var row = table.insertRow(totalRowCount);
+
     
     var cell1 = row.insertCell(0);
     var cell2 = row.insertCell(1);
     var cell3 = row.insertCell(2);
     var cell4 = row.insertCell(3);
-
+    
     cell1.innerHTML = x;
     cell2.innerHTML = y;
     cell3.innerHTML = erro;
